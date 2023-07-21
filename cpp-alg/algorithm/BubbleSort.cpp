f92c162eb3b87bd798d2a348a19290b59832eeaa
elseboy <axuplus@gmail.com>
2023-07-21 12:01:57 +0800

    feat: BubbleSort

--- cpp-alg/algorithm/BubbleSort.cpp
+++ cpp-alg/algorithm/BubbleSort.cpp
@@ -1,35 +1,61 @@
+#include <array>
 #include <iostream>
 #include <vector>
 
-void bubbleSort(std::vector<int> &arr) {
+void vectorBubbleSort(std::vector<int> &arr) {
 
   int size = arr.size();
   bool swapped;
 
-  for (int i = 0; i < size - 1; i++) {
+  for (int i = 0; i < arr.size() - 1; i++) {
     swapped = false;
 
-    for (int j = 0; j < size - i - 1; j++) {
-      if (arr[j] > arr[j + 1]) {
-        std::swap(arr[j], arr[j + 1]);
+    for (int j = 1; j < arr.size() - 1; j++) {
+      if (arr[j] < arr[j - 1]) {
+        int temp;
+        temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
         swapped = true;
       }
     }
 
-    if (!swapped) {
+    if (!swapped)
       break;
+  }
+}
+
+void arrayBubbleSort(std::array<int, 7> &arr) {
+  bool swapped;
+
+  for (int i = 0; i < arr.size() - 1; i++) {
+    for (int j = 1; j < arr.size(); j++) {
+      if (arr[j] > arr[j - 1]) {
+        int temp;
+        temp = arr[j];
+        arr[j] = arr[j - 1];
+        arr[j - 1] = temp;
+        swapped = true;
+      }
     }
+
+    if (!swapped)
+      break;
   }
 }
 
 int main() {
 
-  std::vector<int> numbers = {64, 34, 25, 12, 22, 11, 90};
-
-  bubbleSort(numbers);
-
-  for (int num : numbers)
+  std::vector<int> vectorNumbers = {64, 34, 25, 12, 22, 11, 90};
+  vectorBubbleSort(vectorNumbers);
+  for (int num : vectorNumbers)
     std::cout << num << " ";
 
+  std::cout << std::endl;
+
+  std::array<int, 7> arrayNumbers = {64, 34, 25, 12, 22, 11, 90};
+  arrayBubbleSort(arrayNumbers);
+  for (int num : arrayNumbers)
+    std::cout << num << " ";
   return 0;
 }
