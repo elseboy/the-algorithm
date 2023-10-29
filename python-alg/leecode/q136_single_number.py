d800a513430bb319c6af3ec58f45418a784ec419
elseboy <axuplus@gmail.com>
2023-10-29 22:42:24 +0800

    q136 find single number

---
+++ python-alg/leecode/q136_single_number.py
@@ -0,0 +1,12 @@
+def find_single_number(nums):
+    num_set = set()
+    for i in nums:
+        if i in num_set:
+            num_set.remove(i)
+        else:
+            num_set.add(i)
+    return num_set.pop()
+
+
+nums = [4, 1, 2, 1, 2]
+print(find_single_number(nums))
