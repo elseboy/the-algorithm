0f25ddea419fa228a2516c610680ee57ea39ff62
elseboy <axuplus@gmail.com>
2023-11-02 11:58:54 +0800

    q189 rotate array

---
+++ python-alg/leecode/q189_rotate_array.py
@@ -0,0 +1,14 @@
+def rotate_array(nums, k):
+    n = len(nums)
+    k %= n
+
+    nums.reverse()
+
+    nums[:k] = reversed(nums[:k])
+    nums[k:] = reversed(nums[k:])
+    return nums
+
+
+nums = [1, 2, 3, 4, 5, 6, 7]
+k = 3
+print(rotate_array(nums, k))
