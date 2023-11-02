29d02a10de5c70fc6deac44a2146401af3f9bd82
elseboy <axuplus@gmail.com>
2023-11-02 23:19:03 +0800

    q54 spiral matrix

---
+++ python-alg/leecode/q54_spiral_matrix.py
@@ -0,0 +1,34 @@
+def spiral_matrix(matrix):
+    result = []
+    left, right = 0, len(matrix[0])
+    top, bottom = 0, len(matrix)
+
+    while left < right and top < bottom:
+        # get every i in the top row
+        for i in range(left, right):
+            result.append(matrix[top][i])
+        top += 1
+
+        # get every i in the right col
+        for i in range(top, bottom):
+            result.append(matrix[i][right - 1])
+        right -= 1
+
+        if not (left < right and top < bottom):
+            break
+
+        # get every i in the bottom row
+        for i in range(right - 1, left - 1, -1):
+            result.append(matrix[bottom - 1][i])
+        bottom -= 1
+
+        # get every i in the left col
+        for i in range(bottom - 1, top - 1, -1):
+            result.append(matrix[i][left])
+        left += 1
+
+    return result
+
+
+matrix = [[1, 2, 3], [4, 5, 6], [7, 8, 9]]
+print(spiral_matrix(matrix))
