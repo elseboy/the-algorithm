6b7199d6be068d68d7b1e875c883d6ab63a4fb0a
elseboy <axuplus@gmail.com>
2023-11-08 18:23:03 +0800

    q33 search roated sorted array

---
+++ python-alg/leecode/q33_search_rotated_sorted_array.py
@@ -0,0 +1,28 @@
+def search(nums, target):
+    result = -1
+    l, r = 0, len(nums) - 1
+
+    while l <= r:
+        mid = (l + r) // 2
+        if target == nums[mid]:
+            return mid
+
+        # left sorted portion
+        if nums[mid] >= nums[l]:
+            if target > nums[mid] or target < nums[l]:
+                l = mid + 1
+            else:
+                r = mid - 1
+        # right sorted portion
+        else:
+            if target < nums[mid] or target > nums[r]:
+                r = mid - 1
+            else:
+                l = mid + 1
+    return -1
+
+
+nums = [4, 5, 6, 7, 0, 1, 2]
+target = 0
+
+print(search(nums, target))
