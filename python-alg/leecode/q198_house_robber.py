f963a792ac5e65eb740430ad02728d738c28a7da
elseboy <axuplus@gmail.com>
2023-11-10 11:35:04 +0800

    q198 house robber

---
+++ python-alg/leecode/q198_house_robber.py
@@ -0,0 +1,13 @@
+def rob(nums):
+    rob1, rob2 = 0, 0
+
+    for n in nums:
+        temp = max(n + rob1, rob2)
+        rob1 = rob2
+        rob2 = temp
+
+    return rob2
+
+
+nums = [1, 2, 3, 1]
+print(rob(nums))
