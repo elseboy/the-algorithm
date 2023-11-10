87254de94cdf1deaac0493415f4aba3820c23be7
elseboy <axuplus@gmail.com>
2023-11-10 19:22:05 +0800

    q62 unique paths

---
+++ python-alg/leecode/q62_unique_paths.py
@@ -0,0 +1,16 @@
+def uniquePaths(m, n):
+    row = [1] * n
+    print(row)
+
+    for i in range(m - 1):
+        newRow = [1] * n
+        for j in range(n - 2, -1, -1):
+            newRow[j] = newRow[j + 1] + row[j]
+        row = newRow
+        print(row)
+    return row[0]
+
+
+m = 3
+n = 7
+print(uniquePaths(m, n))
