2fa5f6ece30d1f5489071037b744c37c5c20df63
elseboy <axuplus@gmail.com>
2023-11-11 15:57:07 +0800

    merge two sorted array

---
+++ python-alg/leecode/q88_merged_sorted_array.py
@@ -0,0 +1,20 @@
+def merge_sorted_array(nums1, nums2, m, n):
+    p1, p2, p = m - 1, n - 1, m + n - 1
+
+    while p2 >= 0:
+        if p1 >= 0 and nums1[p1] > nums2[p2]:
+            nums1[p] = nums1[p1]
+            p1 -= 1
+        else:
+            nums1[p] = nums2[p2]
+            p2 -= 1
+        p -= 1
+
+    return nums1
+
+
+nums1 = [1, 2, 3, 0, 0, 0]
+nums2 = [2, 5, 6]
+m = 3
+n = 3
+print(merge_sorted_array(nums1, nums2, m, n))
