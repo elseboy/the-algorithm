349d60d50ca8f0cdfa979997106ef2ae186b0e3c
elseboy <axuplus@gmail.com>
2023-11-16 11:03:37 +0800

    q219 contains duplicate

---
+++ python-alg/leecode/q219_contains_duplicate_II.py
@@ -0,0 +1,14 @@
+def contains_duplicate(nums, k):
+    num_indices = {}
+
+    for i, num in enumerate(nums):
+        if num in num_indices and i - num_indices[num] <= k:
+            return True
+        num_indices[num] = i
+    return False
+
+
+nums = [1, 2, 3, 1]
+k = 3
+
+print(contains_duplicate(nums, k))
