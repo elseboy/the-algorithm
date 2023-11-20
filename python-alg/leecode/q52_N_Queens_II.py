5a2b6c4c03ea48ebba9c44ee15b1cafb6469ea30
elseboy <axuplus@gmail.com>
2023-11-20 22:04:05 +0800

    q52 N Queens II

---
+++ python-alg/leecode/q52_N_Queens_II.py
@@ -0,0 +1,33 @@
+def totalNQueens(n):
+    col = set()
+    posDiag = set()
+    negDiag = set()
+
+    res = 0
+
+    def backtrack(r):
+        if r == n:
+            nonlocal res
+            res += 1
+            return
+
+        for c in range(n):
+            if c in col or (r + c) in posDiag or (r - c) in negDiag:
+                continue
+
+            col.add(c)
+            posDiag.add(r + c)
+            negDiag.add(r - c)
+
+            backtrack(r + 1)
+
+            col.remove(c)
+            posDiag.remove(r + c)
+            negDiag.remove(r - c)
+
+    backtrack(0)
+    return res
+
+
+n = 4
+print(totalNQueens(n))
