5adbf95467058c6a1c31d2e3bb06eb6dd3bfaf6b
elseboy <axuplus@gmail.com>
2023-11-21 10:27:22 +0800

    q918 maximum sum circular subarray

---
+++ python-alg/leecode/q918_maximum_sum_circular_subarray.py
@@ -0,0 +1,13 @@
+def maxSubarraySumCircular(A):
+    total, maxSum, curMax, minSum, curMin = 0, A[0], 0, A[0], 0
+    for a in A:
+        curMax = max(curMax + a, a)
+        maxSum = max(maxSum, curMax)
+        curMin = min(curMin + a, a)
+        minSum = min(minSum, curMin)
+        total += a
+    return max(maxSum, total - minSum) if maxSum > 0 else maxSum
+
+
+nums = [5, -3, 5]
+print(maxSubarraySumCircular(nums))
