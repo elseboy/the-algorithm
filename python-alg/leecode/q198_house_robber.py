7932ceb9ff753169303019a3816f2d1851cefc04
elseboy <axuplus@gmail.com>
2023-11-24 18:57:06 +0800

    add another solution for q198

--- python-alg/leecode/q198_house_robber.py
+++ python-alg/leecode/q198_house_robber.py
@@ -9,5 +9,18 @@ def rob(nums):
     return rob2
 
 
-nums = [1, 2, 3, 1]
-print(rob(nums))
+def dp_solution(nums):
+    dp = [0] * len(nums)
+
+    dp[0] = nums[0]
+    dp[1] = max(nums[0], nums[1])
+
+    for i in range(2, len(nums)):
+        dp[i] = max(dp[i - 1], dp[i - 2] + nums[i])
+        print(dp)
+
+    return dp[-1]
+
+
+nums = [2, 7, 9, 3, 1]
+print(dp_solution(nums))
